139de8a3d7ad290c7e3f238845127842303a817f
Sergey Bakulin <vansergen@gmail.com>
2019-10-03 07:02:49 -0400

    feat: allow to pass all available options to the constructor
    BREAKING CHANGE: port, user, pass and type parameters are not supported 
    in the  constructor and request method supports all available options.

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-const request = require('request-promise-native');
+let request = require('request-promise-native');
 
 class Client {
   /**
-   * @params {Object} options
-   * @params {string} options.url
-   * @params {number} options.port
-   * @params {string} options.user
-   * @params {string} options.pass
-   * @params {number} [options.timeout=10000]
-   * @params {string} [options.type='text/plain']
+   * @params {Object} [options={}]
    */
-  constructor({
-    url,
-    port,
-    user,
-    pass,
-    timeout = 10000,
-    type = 'text/plain'
-  } = {}) {
-    this.url = url;
-    this.port = port;
-    this.user = user;
-    this.pass = pass;
-    this.timeout = timeout;
-    this.type = type;
+  constructor({ ...options } = {}) {
+    request = request.defaults(options);
   }
 
   /**
    * @params {Object} options
    */
   async request(options) {
-    return await request({
-      uri: this.url + ':' + this.port + '/',
-      method: 'POST',
-      headers: { 'content-type': this.type },
-      auth: { user: this.user, pass: this.pass },
-      timeout: this.timeout,
-      body: JSON.stringify(options)
-    });
+    return request(options);
   }
 
   /**
