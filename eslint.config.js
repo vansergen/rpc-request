d629c1d69db4f1f5f9d67f4826bee2976fbf1e09
Sergey Bakulin <sergey.bakulin.github@gmail.com>
2024-12-08 15:04:59 +0000

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import config from "eslint-config-binden-ts";
 export default [
   ...config,
-  {
-    ignores: ["dist/*", "docs/*"],
-    languageOptions: { parserOptions: { project: "tsconfig.json" } },
-  },
+  { ignores: ["dist/*", "docs/*"] },
+  { languageOptions: { parserOptions: { project: "tsconfig.json" } } },
 ];
